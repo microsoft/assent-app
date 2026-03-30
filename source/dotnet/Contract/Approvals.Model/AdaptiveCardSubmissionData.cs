1bccb1f74b4303861fe0e2643e289b1c19daf5ec
Richard Pan <ripan@microsoft.com>
2026-03-30 12:49:48 -0700

    Add Directory.Packages.props file

---
+++ source/dotnet/Contract/Approvals.Model/AdaptiveCardSubmissionData.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.CFS.Approvals.Model;
+
+using Newtonsoft.Json;
+
+/// <summary>
+/// Request payload sent to Approvals plugin.
+/// </summary>
+public class AdaptiveCardSubmissionData
+{
+    [JsonProperty("url", Order = 0)]
+    public string Url { get; set; }
+
+    [JsonProperty("scope", Order = 1)]
+    public string Scope { get; set; }
+
+    [JsonProperty("AskRequest", Order = 2)]
+    public AskRequest AskRequest { get; set; }
+}
